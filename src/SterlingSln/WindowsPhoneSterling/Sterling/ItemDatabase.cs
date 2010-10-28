04db39acba36787e314af887225cb36ad0045a66
jeremylikness_cp <SND\jeremylikness_cp>
2010-10-28 12:14:08 +0000

    Provide new Windows Phone 7 example

---
+++ src/SterlingSln/WindowsPhoneSterling/Sterling/ItemDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Wintellect.Sterling.Database;
+
+namespace WindowsPhoneSterling.Sterling
+{
+    public class ItemDatabase : BaseDatabaseInstance 
+    {
+
+        public override string Name
+        {
+            get { return "ItemDatabase"; }
+        }
+
+        protected override System.Collections.Generic.List<ITableDefinition> _RegisterTables()
+        {
+            return new System.Collections.Generic.List<ITableDefinition>
+            {
+                CreateTableDefinition<ItemViewModel,int>(i=>i.Id)
+            };
+        }
+    }
+}
