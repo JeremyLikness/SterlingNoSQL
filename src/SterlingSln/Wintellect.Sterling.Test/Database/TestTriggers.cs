df84345fc3642a199ca7cd7ebd13f274bc95bb7d
jeremylikness_cp <SND\jeremylikness_cp>
2011-01-14 13:28:30 +0000

    clean up trigger test

--- src/SterlingSln/Wintellect.Sterling.Test/Database/TestTriggers.cs
+++ src/SterlingSln/Wintellect.Sterling.Test/Database/TestTriggers.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using Microsoft.Silverlight.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Wintellect.Sterling.Database;
