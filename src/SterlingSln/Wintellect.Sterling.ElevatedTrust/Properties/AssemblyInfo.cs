8dc3f6409c4c2e2c362c30250b1abdfc4aa55138
petermorlion_cp <SND\petermorlion_cp>
2013-01-27 11:21:24 +0000

    Signing of assemblies

--- src/SterlingSln/Wintellect.Sterling.ElevatedTrust/Properties/AssemblyInfo.cs
+++ src/SterlingSln/Wintellect.Sterling.ElevatedTrust/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6226a809-781a-4b7f-a5ae-0035bf12d0fb")]
 
-[assembly: InternalsVisibleTo("Wintellect.Sterling.ElevatedTrust.Test")]
+[assembly: InternalsVisibleTo("Wintellect.Sterling.ElevatedTrust.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001009306ccd59d15149c6075ebb85c7a0ea29b236dabfacecfb8f5f61d5ae9a384665e8356d6eb6e1dcc3e678ca772624798f6c73ac96af65e14a23fa2be98184508efb91f9a8a99a93571cb84c288f5d24a5144568a76a7e8b2e6c2ac89956fa8e2fa2b04bb1b2abd14a627b1fc9d9230653ee1602f856ddf09451b62f3f3da86fd")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/SterlingSln/Wintellect.Sterling.IsolatedStorage/Properties/AssemblyInfo.cs
+++ src/SterlingSln/Wintellect.Sterling.IsolatedStorage/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: InternalsVisibleTo("Wintellect.Sterling.IsolatedStorage.Test")]
+[assembly: InternalsVisibleTo("Wintellect.Sterling.IsolatedStorage.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001009306ccd59d15149c6075ebb85c7a0ea29b236dabfacecfb8f5f61d5ae9a384665e8356d6eb6e1dcc3e678ca772624798f6c73ac96af65e14a23fa2be98184508efb91f9a8a99a93571cb84c288f5d24a5144568a76a7e8b2e6c2ac89956fa8e2fa2b04bb1b2abd14a627b1fc9d9230653ee1602f856ddf09451b62f3f3da86fd")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
